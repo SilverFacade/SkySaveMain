0cf378b2dc7bb284fc607622b65221baba444c79
luskalbert <ahlusk@uncg.edu>
2024-06-02 15:46:34 -0400

    initial commit

---
+++ src/main/java/com/SkySave/SkySaveMain/SkySaveMainApplication.java
@@ -0,0 +1,13 @@
+package com.SkySave.SkySaveMain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkySaveMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkySaveMainApplication.class, args);
+	}
+
+}
